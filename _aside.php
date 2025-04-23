7252f2287f03bfe26f27c57e5e4bb1ad533dfafa
Marcelo Corrêa Padilha <108826467+lencol-oxidavel@users.noreply.github.com>
2025-04-23 11:34:46 -0300

    a

--- _aside.php
+++ _aside.php
@@ -7,9 +7,9 @@
                     <img class="imga" src="image\SenacAD.png" alt="Anuncio do senac">
                 </div>
             </a>
-            <a target="_blank" href="https://www.senacrs.com.br/home">
+            <a target="_blank" href="">
                 <div class="imgad">
-                    <img class="imga" src="image\SenacAD.png" alt="Anuncio do senac">
+                    <img class="imga" src="image\gago2.png" alt="Anuncio do senac">
                 </div>
             </a>
             <a target="_blank" href="https://www.senacrs.com.br/home">
