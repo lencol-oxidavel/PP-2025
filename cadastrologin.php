2c4fffc85bc78f9f6badc637e0792d8b41849fed
Marcelo Corrêa Padilha <108826467+lencol-oxidavel@users.noreply.github.com>
2025-05-28 10:14:47 -0300

    cadastro ainda incompleto login a fazer

--- cadastrologin.php
+++ cadastrologin.php
@@ -7,6 +7,32 @@
     <link rel="stylesheet" href="./assets/style2.css">
 </head>
 <body>
-    
+
+<div class="containerr">
+    <div class="cadastro">
+        <h2>cadastro</h2>
+        <form class="cad-form" action="">
+            <input type="hidden" name="">
+            <label for="Nome">Nome: </label>
+            <input type="text" name="Nome" required>
+            <label for="Email">Email: </label>
+            <input type="email" name="Email" required>
+            <label for="Senha">Senha: </label>
+            <input type="password" name="Senha" required>
+            <label for="Conf">Confirmar senha:</label>
+            <input type="password" name="Conf" required>
+            <select name="">
+                <option value="" selected disabled>Sexo</option>
+                <option value="F">Feminino</option>
+                <option value="M">Masculino</option>
+            </select>
+            <button class="button" type="submit"><span>Cadastrar</span></button>
+        </form>
+    </div>
+        
+    <div class="login">
+        <form class="log-form" action=""></form>
+    </div>
+</div>
 </body>
 </html>
