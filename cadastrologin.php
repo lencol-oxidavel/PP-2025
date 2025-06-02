21e7529ec0382e549abf29cb2d9faf9d9a69ceb5
Marcelo Corrêa Padilha <108826467+lencol-oxidavel@users.noreply.github.com>
2025-06-02 08:57:20 -0300

    commit

--- cadastrologin.php
+++ cadastrologin.php
@@ -4,24 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./assets/style2.css">
+    <link rel="stylesheet" href="./assets/stylelogin.css">
 </head>
 <body>
 
 <div class="containerr">
     <div class="cadastro">
-        <h2>cadastro</h2>
+        <h2>Cadastro</h2>
         <form class="cad-form" action="">
             <input type="hidden" name="">
             <label for="Nome">Nome: </label>
-            <input type="text" name="Nome" required>
+            <input class="fradius" type="text" name="Nome" required>
             <label for="Email">Email: </label>
-            <input type="email" name="Email" required>
+            <input class="fradius" type="email" name="Email" required>
             <label for="Senha">Senha: </label>
-            <input type="password" name="Senha" required>
+            <input class="fradius" type="password" name="Senha" required>
             <label for="Conf">Confirmar senha:</label>
-            <input type="password" name="Conf" required>
-            <select name="">
+            <input class="fradius" type="password" name="Conf" required>
+            <select class="fradius" name="">
                 <option value="" selected disabled>Sexo</option>
                 <option value="F">Feminino</option>
                 <option value="M">Masculino</option>
@@ -31,6 +31,7 @@
     </div>
         
     <div class="login">
+        <h2>Login</h2>
         <form class="log-form" action=""></form>
     </div>
 </div>
