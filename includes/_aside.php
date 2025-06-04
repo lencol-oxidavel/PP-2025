0b66f094ad5c4e238d6f125467b72a705af4b817
Marcelo Corrêa Padilha <108826467+lencol-oxidavel@users.noreply.github.com>
2025-06-04 09:32:40 -0300

    mais
    cores que eram usadas para contraste entre aside e main foram alteradas para as cores oficiais, ajustei o tamanho das imagens para deixar mais bonito

--- includes/_aside.php
+++ includes/_aside.php
@@ -2,21 +2,21 @@
 
     <aside>
         <div class="sidebar">
-            <a target="_blank" href="https://www.senacrs.com.br/home">
-                <div class="imgad">
-                    <img class="imga" src="image\SenacAD.png" alt="Anuncio do senac">
-                </div>
-            </a>
-            <a target="_blank" href="">
-                <div class="imgad">
-                    <img class="imga" src="image\gago2.png" alt="Anuncio do senac">
-                </div>
-            </a>
-            <a target="_blank" href="https://www.senacrs.com.br/home">
-                <div class="imgad">
-                    <img class="imga" src="image\SenacAD.png" alt="Anuncio do senac">
-                </div>
-            </a>
+            <div class="imgad">
+                <a target="_blank" href="https://www.senacrs.com.br/home">
+                    <img class="imga" src="image/SenacAD.png" alt="Anuncio do senac">
+                </a>
+            </div>
+            <div class="imgad">
+                <a target="_blank" href="">
+                    <img class="imga" src="image/gago2.png" alt="Anuncio do senac">
+                </a>
+            </div>
+            <div class="imgad">
+                <a target="_blank" href="https://www.senacrs.com.br/home">
+                    <img class="imga" src="image/SenacAD.png" alt="Anuncio do senac">
+                </a>
+            </div>
         </div>
     </aside>
 </div>
