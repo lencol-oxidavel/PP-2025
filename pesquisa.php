b35c23326cc163000c9bfde8a5069db966579c06
Marcelo Corrêa Padilha <108826467+lencol-oxidavel@users.noreply.github.com>
2025-06-04 08:29:21 -0300

    voltar em pesquisa

--- pesquisa.php
+++ pesquisa.php
@@ -9,6 +9,9 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=search" />
 </head>
 <body>  
+
+    <a href="javascript:history.back()" class="voltar">&#10229; Voltar</a>
+
     <form>
         <div class="search">
            <span class="search-icon material-symbols-outlined">search</span>
