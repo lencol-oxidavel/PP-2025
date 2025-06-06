153eedbff6296bba844b3738f97689b306918f0f
Marcelo Corrêa Padilha <108826467+lencol-oxidavel@users.noreply.github.com>
2025-06-06 08:23:30 -0300

    VAZIO VOID NULO NADA ZERO INEXISTÊNCIA
    void

--- pesquisa.php
+++ pesquisa.php
@@ -18,15 +18,28 @@
            <input class="search-input" type="search" placeholder="Pesquisar">
         </div>
     </form>
+<!-- usar while loop do php com banco de dados para mostrar os 10 resultados mais parecidos -->
     <div class="Resultado">
         <div class="Resultado-img">
             <img src="./image/gago2.png" alt="Imagem do resultado">
         </div>
-        <!-- usar while loop do php com banco de dados para mostrar os 10 resultados mais parecidos -->
+        
         <div class="Resultado-Texto">
             <h2>Gato Fofo é flagrado durante bocejo.</h2>
             <p>
                 O gato boceja um bocejo bocejante, caracterizado por um bocejar contínuo, prolongado, estendido, onde ele boceja enquanto realiza o ato do bocejo, que é, por definição, o bocejo do bocejo sendo bocejado, no momento em que o bocejo acontece, ocorre, se manifesta, se expressa, se revela, em sua forma mais bocejável de bocejo. Nesse exato instante, precisamente nesse momento específico e pontual do bocejo em andamento, o dono, o proprietário, o tutor, a pessoa responsável pelo gato, utiliza um dispositivo fotográfico, ou seja, uma câmera, um aparelho de captura de imagens, para tirar, registrar, capturar, eternizar uma foto, uma fotografia, uma imagem visual, da cena visual do bocejo bocejado pelo gato em pleno bocejo, clicando no botão de captura da câmera no momento em que o bocejo está bocejando, durante o bocejo, no meio do bocejo, enquanto o bocejo está em pleno bocejo..
+            </p>
+        </div>
+    </div>
+    <div class="Resultado">
+        <div class="Resultado-img">
+            <img src="./image/gagov2.png" alt="Imagem do resultado">
+        </div>
+        
+        <div class="Resultado-Texto">
+            <h2>VOID</h2>
+            <p>
+                VOID
             </p>
         </div>
     </div>
