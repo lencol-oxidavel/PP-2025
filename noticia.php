bfab877f992009f02f6fe26977c9f7e60f71cd2d
Marcelo Corrêa Padilha <108826467+lencol-oxidavel@users.noreply.github.com>
2025-07-08 08:37:58 -0300

    Create noticia.php

---
+++ noticia.php
@@ -0,0 +1,18 @@
+<?php
+include_once './includes/_header.php';
+?>
+
+    <div class="maincontainer">
+        <main>
+            
+        </main>
+
+
+
+
+<?php
+include_once './includes/_aside.php';
+?>
+<?php
+include_once './includes/_footer.php';
+?>
