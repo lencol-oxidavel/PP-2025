701638b69455dbead5b1a58b4f5174901dee3efa
Pedro <h3enr1quepedr011@gmail.com>
2025-07-08 09:33:14 -0300

    Textos

--- metodologia.php
+++ metodologia.php
@@ -4,7 +4,9 @@ include_once './includes/_header.php';
 
 <div class="maincontainer">
     <main>
-        <p>oi</p>
+        
+        <h1>A metodologia</h1>
+        <p>Nossa metodologia utilizada será uma pesquisa quantitativa sobre a Interface cérebro-máquina (BMI), utilizando artigos no Google acadêmico e informações públicas disponibilizadas sobre o chip da empresa Neuralink. </p>
     </main>
 
 <?php
--- sobrenos.php
+++ sobrenos.php
@@ -5,7 +5,8 @@ include_once './includes/_header.php';
 
 <div class="maincontainer">
     <main>
-        <p>oi</p>
+        <h1>Quem somos</h1>
+        <p>Somos estudantes do técnico profissionalizante com foco em tecnologia do senac distrito criativo, com objetivo em ampliar os conhecimentos gerais das pessoas sobre o assunto que tanto nos interessa.</p>
     </main>
  
 <?php
