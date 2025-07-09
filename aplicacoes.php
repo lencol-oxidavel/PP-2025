527220e9912c1138c1e04b4f8b45af516c0f59ad
Pedro <h3enr1quepedr011@gmail.com>
2025-07-09 12:19:40 -0300

    textos²

--- aplicacoes.php
+++ aplicacoes.php
@@ -4,8 +4,11 @@ include_once './includes/_header.php';
 
 <div class="maincontainer">
     <main>
-        <h1>Neuralink</h1>
-        <p></p>
+        <h1>Utilizações da Neuralink</h1>
+        <p>Sabendo que a empresa tem o objetivo de desenvolver uma BMI (Brain machine interface), o dispositivo será designado a auxiliar a restaurar funções motoras e transtornos neurológicos de pessoas que necessitam do auxílio. </p>
+        <p>    Implantado através de cirurgias realizadas por roboôs da própria empresa, o dispositivo possuí diversas matrizes, cada uma possuindo 96 pequenos fios de eletrodos flexiveis com 32 eletrodos independentes por fio, totalizando 3072.
+            A utilização desses fios permite aumentar a precisão, classificação e interpretação da atividade elétrica cerebral.
+        </p>
     </main>
 
 <?php
