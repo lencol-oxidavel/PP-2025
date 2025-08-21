b3533e29f02fe96eaae64ebc3ad5aaa26f3d57a1
PH <138227579+PedroHenriquePR@users.noreply.github.com>
2025-08-21 12:20:39 -0300

    Footer bizarro (poha marcelo)

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,5 +1,12 @@
     
+    
     <footer>
+        <div class="Teu_Pai">
+        <a href="https://neuralink.com/" target="_blank">Neuralink</a>
+        </div>
+        
+
+
     </footer>
 
     <script src="./assets/script.js"></script>
