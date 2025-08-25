d8320ced1d8f8ef8dc19b45f59ff1f84babcecc3
PH <138227579+PedroHenriquePR@users.noreply.github.com>
2025-08-25 11:03:33 -0300

    footer

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,5 +1,14 @@
     
     <footer>
+        <div class="Container1">  
+            <a class="Neuralinka" href="https://neuralink.com/" target="_blank">
+                Neuralink
+            </a>
+        </div>
+    
+
+
+
     </footer>
 
     <script src="./assets/script.js"></script>
