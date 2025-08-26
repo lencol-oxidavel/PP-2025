1dc92f257f34c7b6444bf04cd78e369af07ba100
PH <138227579+PedroHenriquePR@users.noreply.github.com>
2025-08-26 09:08:35 -0300

    footer

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,10 +1,18 @@
     
     <footer>
         <div class="Container1">  
+            
+        <p>Conheça o trabalho oficial da empresa Neuralink: </p>
+        
             <a class="Neuralinka" href="https://neuralink.com/" target="_blank">
                 Neuralink
             </a>
-        </div>
+            
+            <p>Conheça nossa escola: </p>
+            <a class="Neuralinka" href="https://www.senacrs.com.br/escola/8ca9e0ba-6659-45f6-b22b-ce55558abbae" target="_blank">
+               Senac
+              </a>        
+            </div>
     
 
 
